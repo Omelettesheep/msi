a424314bf91b1ff87506a6059d5c8aa15e05f846
omelette <1875840145@qq.com>
2017-07-11 23:33:23 +0800

    修改header

--- components/header/header.js
+++ components/header/header.js
@@ -5,6 +5,7 @@
 
 	var defaults = {
 		defaultIcon:'glyphicon glyphicon-tasks',
+		userid:93109,
 		userName:(function(){
 			if(window.localStorage){
 				return window.localStorage.getItem('misUserName')||'未登录'
@@ -39,7 +40,7 @@
 				var storage = window.localStorage
 			}
 			
-			this.headerStr = ' <header class="main-header"><div class="title"><h1>'+this.settings.systemName+'</h1></div><div class="user"><div class="dropdown"><div class="dropdown-toggle user-action" id="dropdownMenu1" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i>'+this.settings.userName+'<span class="caret"></span></div><ul class="dropdown-menu text-right" role="menu" aria-labelledby="dropdownMenu1"><li role="presentation"><a role="menuitem" tabindex="-1" href="../login.html">退出登录</a></li></ul></div></div><ul class="right-nav"><li><div class="back"><a href="home.html"><i class="glyphicon glyphicon-home"></i><span>mis菜单</span></a></div></li></ul><div class="clearfix"></div></header>';
+			this.headerStr = ' <header class="main-header"><div class="title"><h1>'+this.settings.systemName+'</h1></div><div class="user"><div class="dropdown"><div class="dropdown-toggle user-action" id="dropdownMenu1" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i>'+this.settings.userName+'<span class="caret"></span></div><ul class="dropdown-menu text-right" role="menu" aria-labelledby="dropdownMenu1"><li role="presentation"><a role="menuitem" tabindex="-1" href="../login.html">退出登录</a></li></ul></div></div><ul class="right-nav"><li><div class="back"><a href="home.html"><i class="glyphicon glyphicon-home"></i><span>mis菜单</span></a></div></li><li><div class="back"><i class="glyphicon glyphicon-home"></i><span>员工号:'+this.settings.userid+'</span></a></div></li></ul><div class="clearfix"></div></header>';
 
 			this.appendElement();
 		},
